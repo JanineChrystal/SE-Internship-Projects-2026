76fb110f89643bb5c5e393c9cbaab1a9d5f17ad8
JanineChrystal <chrystalampusta@gmail.com>
2026-06-22 20:47:50 +0800

    refactor(button): transferred the gradient style to global.css and inserted it in the classname of the button.

--- janine-portfolio/src/components/buttons/button.tsx
+++ janine-portfolio/src/components/buttons/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = ({ children, className = "", ...props }: ButtonProps) => {
 	return (
 		<button
-			className={`bg-linear-to-r from-(--color-brand-yellow)] to-yellow-300 text-black font-black text-xl px-10 py-4 rounded-full shadow-lg hover:scale-105 transition-transform ${className}`}
+			className={`btn-gradient-brand text-black font-black text-xl px-10 py-4 rounded-full shadow-md shadow-black hover:scale-105 transition-transform ${className}`}
 			{...props}
 		>
 			{children}

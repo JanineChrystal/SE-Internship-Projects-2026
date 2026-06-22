d4d98f2b2942a88ec17081168a2e4f418383515f
JanineChrystal <chrystalampusta@gmail.com>
2026-06-22 19:33:18 +0800

    feat(button): added reusable general button component

---
+++ janine-portfolio/src/components/buttons/button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
+}
+
+const Button = ({ children, className = "", ...props }: ButtonProps) => {
+	return (
+		<button
+			className={`bg-linear-to-r from-(--color-brand-yellow)] to-yellow-300 text-black font-black text-xl px-10 py-4 rounded-full shadow-lg hover:scale-105 transition-transform ${className}`}
+			{...props}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
